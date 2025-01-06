fcc92b439aa445a8684da739e82e7f0320b5f1c8
ReyXX777 <cse2021100@rcciit.org.in>
2025-01-06 23:43:41 +0530

    Update Navbar.js
    edit1

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Hotel Booking</h1>
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h1>Hotel Booking</h1>
+      </div>
+      <ul className="navbar-menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#rooms">Rooms</a></li>
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
     </nav>
   );
 };
